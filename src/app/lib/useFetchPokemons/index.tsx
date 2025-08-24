a4eb2eea59137a193c95a9fdb8aa314f7f1f51c6
Vinicius Alves Pereira <vinicius.apereira04@gmail.com>
2025-08-24 17:59:41 -0300

    fix: load state

--- src/app/lib/useFetchPokemons/index.tsx
+++ src/app/lib/useFetchPokemons/index.tsx
@@ -6,6 +6,8 @@ export const useFetchPokemons = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+
   const offset = useRef(0);
   const limit = useRef(20);
 
@@ -14,6 +16,7 @@ export const useFetchPokemons = () => {
   const getPokemonsByName = useCallback(
     async (name: string): Promise<Pokemon[]> => {
       try {
+        console.log(nameOffset.current);
         const maxLimit = 10000;
 
         const res = await fetch(
@@ -24,29 +27,39 @@ export const useFetchPokemons = () => {
 
         const pokemons: Pokemon[] = [];
 
+        let countResult = 0;
+
         for (let index = 0; index < json.results.length; index++) {
           const pokemon = json.results[index];
 
           if (name && !pokemon.name.toLowerCase().includes(name.toLowerCase()))
             continue;
 
-          const details = (await (
-            await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-          ).json()) as PokemonDetailsResponse;
+          try {
+            const details = (await (
+              await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+            ).json()) as PokemonDetailsResponse;
 
-          pokemons.push({
-            id: details.id,
-            name: pokemon.name,
-            url: details.sprites.front_default,
-            types: details.types.map((type) => type.type.name),
-          });
+            pokemons.push({
+              id: details.id,
+              name: pokemon.name,
+              url: details.sprites.front_default,
+              types: details.types.map((type) => type.type.name),
+            });
 
-          console.log(pokemons.length);
-
-          if (pokemons.length >= 10) {
-            nameOffset.current += index + 1;
-            break;
+            if (pokemons.length >= 10) {
+              countResult = index + 1;
+              break;
+            }
+          } catch (error) {
+            console.error("Error fetching pokemon details:", error);
           }
+        }
+
+        if (countResult !== 0) {
+          nameOffset.current += countResult;
+        } else {
+          nameOffset.current = 10000;
         }
 
         return pokemons;
@@ -61,7 +74,7 @@ export const useFetchPokemons = () => {
   const getPokemons = useCallback(async (): Promise<Pokemon[]> => {
     try {
       const res = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?offset=${offset.current}&limit=${limit.current}`
+        `https://pokeapi.co/api/v2/pokemon-species?offset=${offset.current}&limit=${limit.current}`
       );
 
       const json = (await res.json()) as PokemonResponse;
@@ -71,16 +84,20 @@ export const useFetchPokemons = () => {
       for (let index = 0; index < json.results.length; index++) {
         const pokemon = json.results[index];
 
-        const details = (await (
-          await fetch(pokemon.url)
-        ).json()) as PokemonDetailsResponse;
+        try {
+          const details = (await (
+            await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+          ).json()) as PokemonDetailsResponse;
 
-        pokemons.push({
-          id: details.id,
-          name: pokemon.name,
-          url: details.sprites.front_default,
-          types: details.types.map((type) => type.type.name),
-        });
+          pokemons.push({
+            id: details.id,
+            name: pokemon.name,
+            url: details.sprites.front_default,
+            types: details.types.map((type) => type.type.name),
+          });
+        } catch (error) {
+          console.error("Error fetching pokemon details:", error);
+        }
       }
 
       return pokemons;
@@ -93,6 +110,7 @@ export const useFetchPokemons = () => {
   const fetchPokemonsByName = useCallback(
     async (name: string): Promise<void> => {
       setIsLoading(true);
+      nameOffset.current = 0;
       const pokemons = await getPokemonsByName(name);
       setIsLoading(false);
       setPokemons(pokemons);
@@ -102,9 +120,12 @@ export const useFetchPokemons = () => {
 
   const loadMore = useCallback(
     async (search: string) => {
+      setIsLoadingMore(true);
+
       if (search.length) {
         const pokemons = await getPokemonsByName(search);
         setPokemons((prev) => [...prev, ...pokemons]);
+        setIsLoadingMore(false);
         return;
       }
 
@@ -112,6 +133,7 @@ export const useFetchPokemons = () => {
       const pokemons = await getPokemons();
 
       setPokemons((prev) => [...prev, ...pokemons]);
+      setIsLoadingMore(false);
     },
     [getPokemons, getPokemonsByName]
   );
@@ -127,5 +149,5 @@ export const useFetchPokemons = () => {
     loadInitial();
   }, [getPokemons]);
 
-  return { loadMore, fetchPokemonsByName, pokemons, isLoading };
+  return { loadMore, fetchPokemonsByName, pokemons, isLoading, isLoadingMore };
 };
