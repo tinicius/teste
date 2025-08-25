f7bb9b60e76dee9619da704ff0ed5be8d8bac130
Vinicius Alves Pereira <vinicius.apereira04@gmail.com>
2025-08-25 14:41:58 -0300

    fix: error component

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,6 +1,5 @@
-import { PokemonCard } from "./PokemonCard";
 import { PokemonImages } from "./PokemonImages";
 import { PokemonModal } from "./PokemonModal";
 import { PokemonTypeCard } from "./PokemonTypeCard";
 
-export { PokemonCard, PokemonImages, PokemonModal, PokemonTypeCard };
+export { PokemonImages, PokemonModal, PokemonTypeCard };
